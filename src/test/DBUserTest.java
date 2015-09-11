4a1925181923f679d23eed95e2edcb82c9a03517
KenNgo <phucuongngo@gmail.com>
2015-09-11 10:16:34 -0400

    add integration testing

--- src/test/DBUserTest.java
+++ src/test/DBUserTest.java
@@ -1,10 +1,15 @@
 package test;
 
 import static org.junit.Assert.*;
+import model.Shoplineitem;
+import model.Shoporder;
+import model.Shopproduct;
 import model.Shopuser;
 
 import org.junit.Test;
 
+import db.DBLineItem;
+import db.DBProduct;
 import db.DBUser;
 
 public class DBUserTest
@@ -78,6 +83,42 @@ public class DBUserTest
 		user.setUserName("admiN");
 		user.setEmail("admin2@infosys.com");
 		assertFalse(DBUser.isAvailable(user));
+	}
+	
+	@Test
+	public void testLoginAndAddItemToCart()
+	{
+		System.out.println("Test login and add one line item...");
+		Shopuser user = DBUser.getUser(3);
+		System.out.println(user.getFullName());
+		
+		//test has active order
+		assertTrue(user.hasActiveOrder());
+		
+		//get active order
+		Shoporder activeCart = user.getActiveOrder();
+		
+		//get current size of cart
+		int numItems = activeCart.getShoplineitems().size();
+		System.out.println("current numItems = " + numItems);
+		
+		System.out.println("adding one line to the cart ....");
+		//get product from db
+		Shopproduct product = DBProduct.getProduct(2);
+		
+		//add one item to the cart
+		Shoplineitem newLine = new Shoplineitem();
+		newLine.setQuantity(7);
+		newLine.setReturned("No");
+		newLine.setShoporder(activeCart);
+		newLine.setShopproduct(product);
+		
+		DBLineItem.insert(newLine);
+		System.out.println("Added");
+		//get active order again
+		activeCart = user.getActiveOrder();
+		System.out.println("new NumItems is = " + activeCart.getShoplineitems().size());
+		assertTrue(activeCart.getShoplineitems().size() == (numItems + 1));
 	}
 	
 
